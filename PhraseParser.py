c2db8a6a84842660272112c7b2afa30dc5cd4576
Justin Roll <justin.j.roll@gmail.com>
2014-10-24 16:06:19 -0700

    Added a barebones PhraseParser class

---
+++ PhraseParser.py
@@ -0,0 +1,18 @@
+import nltk
+
+class SupportedPhrases:
+    SVO = "subject verb object"
+    AN = "adjective noun"
+
+class PhraseParser:
+
+    #tokenize a phrase, tag it, and then return an object with tags for each word and other metadata
+    def parsePhrase(self, phrase):
+        pass
+    
+    #Give each item in the phrase a POS tag
+    def tagPhrase(self, phrase):
+        pass
+
+
+    
